61db8dc2b0330ac4d7a06a569d3804467545daad
M.Deepak <sm4381437@gmail.com>
2025-12-14 19:55:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ particlesJS('particles-js', {
             enable: true,
             distance: 150,
             color: '#00d4ff',
-            opacity: 0.4,
+            opacity: 0.5,
             width: 1
         },
         move: {
@@ -176,12 +176,21 @@ document.getElementById('contactForm').addEventListener('submit', function (e) {
     // Get form values
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    const subject = document.getElementById('subject').value;
     const message = document.getElementById('message').value;
+
+    //  send-email  
+    var templateParams = {
+        name: name,
+        email: email,
+		message: message
+      };
+
+      emailjs.send('service_uzqmdps', 'template_u28w0xn', templateParams)
+      alert(`Thank you ${name}! Your message has been sent. We'll get back to you at ${email} soon!`);
 
     // Here you would typically send the form data to a server
     // For now, we'll just show a success message
-    alert(`Thank you ${name}! Your message has been sent. We'll get back to you at ${email} soon!`);
+    // alert(`Thank you ${name}! Your message has been sent. We'll get back to you at ${email} soon!`);
 
     // Reset form
     this.reset();
